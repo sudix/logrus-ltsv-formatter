6c1080d98d8187596ae69f276bb3f0f4b886fa14
sudix <sudix7755@gmail.com>
2015-08-24 19:33:25 +0900

    Delete old implement method

--- ltsv_formatter.go
+++ ltsv_formatter.go
@@ -5,7 +5,6 @@ import (
 	"bytes"
 	"fmt"
 	"sort"
-	"strings"
 
 	"github.com/Sirupsen/logrus"
 )
@@ -54,31 +53,4 @@ func (f *LogrusLTSVFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	buf.WriteString(fmt.Sprintf("msg:%s\n", entry.Message))
 
 	return buf.Bytes(), nil
-}
-
-func (f *LogrusLTSVFormatter) Format2(entry *logrus.Entry) ([]byte, error) {
-	var buf []string
-	var keys []string
-
-	for k, _ := range entry.Data {
-		keys = append(keys, k)
-	}
-
-	sort.Strings(keys)
-
-	timestampFormat := f.TimestampFormat
-	if timestampFormat == "" {
-		timestampFormat = logrus.DefaultTimestampFormat
-	}
-
-	buf = append(buf, fmt.Sprintf("time:%s", entry.Time.Format(timestampFormat)))
-	buf = append(buf, fmt.Sprintf("level:%s", entry.Level.String()))
-
-	for _, k := range keys {
-		buf = append(buf, fmt.Sprintf("field.%s:%v", k, entry.Data[string(k)]))
-	}
-
-	buf = append(buf, fmt.Sprintf("msg:%s\n", entry.Message))
-
-	return []byte(strings.Join(buf, "\t")), nil
 }
